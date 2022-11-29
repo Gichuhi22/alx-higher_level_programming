8015736438127e8c4e0d35d212a61d70182c43e9
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-11-29 21:04:46 +0000

    modified file 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-for number in range(0, 10):
-    for num in range(0,10):
-        if num == 9 and number == 9:
-            print("{}{}".format(number, num))
-        else:
-            print("{}{}, ".format(number, num), end="")
+for number in range(0, 99):
+    print("{0:02d}".format(number), end=", ")
+
+print("{}".format(99))
