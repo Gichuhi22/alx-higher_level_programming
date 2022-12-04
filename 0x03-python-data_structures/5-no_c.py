86d3bbebf78aa0ce631c8a4c3ea9d8cb37725782
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-12-04 20:12:16 +0000

    added the file 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        new_string = ""
+        new_string = ''
         my_string = my_string.lower()
 
         for item in my_string:
-            if item == 'c':
+            if item == "c":
                 continue
             else:
                 new_string += item
