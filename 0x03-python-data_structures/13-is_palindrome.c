a962ee8bb84ea79a4cd002cbf93fe4e5d14d1aaa
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-12-05 19:12:31 +0000

    added the file 13-is_palindrome.c, lists.h

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,51 @@
+#include"lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *is_palindrome - checks if a linked list is a palindrome
+ *@head: pointer to first node
+ *Return: returns 1 if palindrome or 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	int num_nodes = 0, i = 0, j, x;
+	listint_t *temp, *curr;
+	int *num_array;
+
+	temp = curr = *head;
+	if (*head == NULL)
+		return (1);
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		num_nodes++;
+	}
+	num_array = malloc(sizeof(int) * (num_nodes + 1));
+	if (!num_array)
+	{
+		free(num_array);
+		exit(1);
+	}
+
+	while (curr != NULL)
+	{
+		num_array[i] = curr->n;
+		curr = curr->next;
+		i++;
+	}
+	for (j = 0, i = (num_nodes - 1); j < num_nodes / 2; j++, i--)
+	{
+		if (num_array[j] != num_array[i])
+		{
+			x = 0;
+			break;
+		}
+		else
+		{
+			x = 1;
+		}
+	}
+	free(num_array);
+	return (x);
+}
