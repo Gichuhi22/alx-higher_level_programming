706906f38023cbbf65164b237e3d426ebda80d07
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-12-05 13:01:43 +0000

    added new file 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence:
+        new_tuple = (len(sentence), sentence[0])
+    else:
+        new_tuple = (0, None)
+    return new_tuple
