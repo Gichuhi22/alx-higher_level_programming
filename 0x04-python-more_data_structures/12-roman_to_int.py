c9009a102be6041684775f23f95b24b8cddf93fa
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-12-11 20:19:46 +0000

    added 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if roman_string:
+        roman_string = roman_string.lower()
+        roman = {"i": 1, "v": 5, "x": 10, "l": 50,
+                 "c": 100, "d": 500, "m": 1000}
+        i = len(roman_string) - 1
+        output = 0
+        while i >= 0:
+            if i < len(roman_string) - 1 and
+            roman[roman_string[i]] < roman[roman_string[i + 1]]:
+
+                output -= roman[roman_string[i]]
+            else:
+                output += roman[roman_string[i]]
+            i -= 1
+        return (output)
+    else:
+        return (0)
