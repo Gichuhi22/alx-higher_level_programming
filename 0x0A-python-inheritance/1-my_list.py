f46edbe9f6ad7a645d1d5a205258288af2e59ec7
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2023-01-11 21:12:31 +0000

    added new files

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" My_list module
+    defines a class that inherits from a list
+"""
+
+class MyList(list):
+    """defines a class MyList that inherits from list.
+    """
+    def print_sorted(self):
+        """prints elements in a list in ascending order"""
+
+        sortedlist = sorted(self)
+        print(sortedlist)
