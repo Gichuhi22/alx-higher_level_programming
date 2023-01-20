8dff6bccb13ab9e4a1c469dbce383e7c04f085e6
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2023-01-20 19:43:47 +0000

    updated rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,39 +107,9 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-    
+
     def area(self):
         """
             Returns the are of an instance rectangle
         """
         return self.__width * self.__height
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
