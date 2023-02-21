f454fdb03aaf777c1cdbbde08dbd6767013cdfa5
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2023-02-21 09:27:21 +0000

    modified

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- change database to utf-8
 -- Use hbtn_0c_0 database
-USE hbtn_0c_0;
+-- USE hbtn_0c_0;
 
 -- Change database to utf-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
